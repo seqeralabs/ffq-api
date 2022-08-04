7c5e652772fb90c311caf8430f156aadbd9670d1
Ben Sherman <bentshermann@gmail.com>
2022-08-04 16:05:47 -0400

    Update query endpoint to match CLI (#9)

--- src/ffq_api/app.py
+++ src/ffq_api/app.py
@@ -4,20 +4,21 @@ import argparse
 from datetime import datetime
 from enum import Enum
 import re
-from typing import Union
+from typing import Optional
 
 from fastapi import FastAPI, Request
 from . import __version__
 from ffq import __version__ as ffq_version
 from ffq import main as ffq_main
 
-# Build an Enum class for FastAPI that dynamically
-# contains the ffq SEARCH_TYPES
-class TempEnum(str, Enum):
+
+# Define enum types for the API endpoint
+class NamedEnum(str, Enum):
     pass
 
+LinkType = NamedEnum("LinkType", {v: v for v in ["ftp", "aws", "gcp", "ncbi"]})
+SearchType = NamedEnum("SearchType", {v: v for v in ffq_main.SEARCH_TYPES})
 
-Search_Types = TempEnum("Search_Types", {st: st for st in ffq_main.SEARCH_TYPES})
 
 # Initialise the FastAPI app
 app = FastAPI()
@@ -38,47 +39,54 @@ def read_root(request: Request):
 @app.get("/v1alpha1/{accession_str}")
 def read_item(
     accession_str: str,
-    search_type: Union[Search_Types, None] = None,
-    ftp: bool = False,
-    aws: bool = False,
-    gcp: bool = False,
-    ncbi: bool = False,
-    level: Union[int, None] = None,
+    search_type: Optional[SearchType] = None,
+    level: Optional[int] = None,
+    links: Optional[LinkType] = None,
 ):
+    """
+    Perform a query with ffq.
+
+    :param accession_str: One or multiple SRA / GEO / ENCODE / ENA / EBI-EMBL / DDBJ / Biosample accessions, DOIs, or paper titles
+    :param search_type:   UNUSED
+    :param level:         Max depth to fetch data within accession tree
+    :param links:         Retrieve only links from one of 'ftp', 'aws', 'gcp', or 'ncbi'
+    """
     request_start = datetime.now()
-    accessions = re.split(";| |,", accession_str)
 
+    # Parse arguments
     args = argparse.Namespace()
-    args.IDs = accessions  # One or multiple SRA / GEO / ENCODE / ENA / EBI-EMBL / DDBJ / Biosample accessions DOIs, or paper titles
-    args.o = None  # Path to write metadata (default: standard out)
+    args.IDs = re.split(";| |,", accession_str)
+    args.o = None
     args.t = search_type
-    args.l = level  # Max depth to fetch data within accession tree
-    args.ftp = ftp  # Return FTP links
-    args.aws = aws  # Return AWS links
-    args.gcp = gcp  # Return GCP links
-    args.ncbi = ncbi  # Return NCBI links
-    args.split = False  # Split output into separate files by accession
-    args.verbose = False  # Print debugging information
+    args.l = level
+    args.ftp = (links == "ftp")
+    args.aws = (links == "aws")
+    args.gcp = (links == "gcp")
+    args.ncbi = (links == "ncbi")
+    args.split = False
+    args.verbose = False
 
+    # Perform query
     results = ffq_main.run_ffq(args)
-    request_finish = datetime.now()
-    results["meta"] = {
-        "ffq_api_version": __version__,
-        "ffq_version": ffq_version,
-        "query": {
-            "IDs": accession_str,
-            "search_type": search_type,
-            "level": level,
-            "ftp": ftp,
-            "aws": aws,
-            "gcp": gcp,
-            "ncbi": ncbi,
-        },
-        "request": {
-            "start": request_start.isoformat(),
-            "finish": request_finish.isoformat(),
-            "duration_seconds": (request_finish - request_start).total_seconds(),
-        },
-    }
 
-    return results
+    request_finish = datetime.now()
+
+    # Return API response
+    return {
+        "results": results,
+        "meta": {
+            "ffq_api_version": __version__,
+            "ffq_version": ffq_version,
+            "query": {
+                "IDs": accession_str,
+                "search_type": search_type,
+                "level": level,
+                "links": links,
+            },
+            "request": {
+                "start": request_start.isoformat(),
+                "finish": request_finish.isoformat(),
+                "duration_seconds": (request_finish - request_start).total_seconds(),
+            },
+        }
+    }
